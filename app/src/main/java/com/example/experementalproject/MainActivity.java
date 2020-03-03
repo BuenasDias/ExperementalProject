0edb9800015486c2daee4ecf12a334564002bc1e
BuenasDias <tmaksim1989@gmail.com>
2020-03-03 22:33:21 +0300

    Animation button

--- app/src/main/java/com/example/experementalproject/MainActivity.java
+++ app/src/main/java/com/example/experementalproject/MainActivity.java
@@ -3,12 +3,31 @@ package com.example.experementalproject;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+import android.widget.ImageView;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    Animation topAnim, botAnim;
+    ImageView imageView;
+    Button btnStart;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
+
+        topAnim = AnimationUtils.loadAnimation(this, R.anim.top_animation);
+        botAnim = AnimationUtils.loadAnimation(this, R.anim.bottom_animation);
+
+        btnStart = (Button) findViewById(R.id.btnStart);
+
+        btnStart.setAnimation(botAnim);
+
     }
 }
